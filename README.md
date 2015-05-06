9f0e216e9c2c6d02215edf6b9118cddab0b2bdb6
U-APOLLOGROUP\ihamilto <ihamilto@USPHXAWNR9YHWAA.apollogrp.edu>
2015-05-06 09:28:32 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# report_luminis
-Moodle report for Luminis integration records
+# report_luminis Plugin (Luminis Reporting)
+### Moodle report for Luminis integration records
+
+This is a basic report that queries against the integration metadata.
+
+place luminis directory in <moodle_root>/report/
---
+++ luminis/lang/en/report_luminis.php
@@ -0,0 +1,60 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lang strings
+ *
+ * @package    report
+ * @subpackage luminis
+ * @copyright  2015 Ian Hamilton
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+ $string['pluginname'] = 'Luminis Integration Report';
+ $string['reportsettings'] = 'Luminis Message Broker Report';
+ $string['intro'] = 'This report displays the Luminis Message Broker importing data history';
+ $string['messagetype'] = 'Message Type';
+ $string['hoursback'] = 'Hours Since Import';
+ $string['lookup'] = 'CRN/Student-ID Filter';
+ $string['getreport'] = 'Get the report';
+ $string['luminis:view'] = 'View Luminis Report';
+ $string['personreporttitle'] = 'Luminis Person Integration Report Result';
+ $string['coursereporttitle'] = 'Luminis Course Integration Report Result';
+ $string['enrolreporttitle'] = 'Luminis Enrolment Integration Report Result';
+ $string['termreporttitle'] = 'Luminis Term Integration Report Result';
+ $string['sourcedid'] = 'Luminis Source Id';
+ $string['fullname'] = 'Full Name';
+ $string['email'] = 'Email';
+ $string['username'] = 'Banner User Name';
+ $string['coursenumber'] = 'Course Number';
+ $string['term'] = 'Term';
+ $string['fulltitle'] = 'Full Title';
+ $string['num'] = 'Section Number';
+ $string['section'] = 'Section';
+ $string['timemodified'] = 'Time Modified';
+ $string['role'] = 'Course Role';
+ $string['midtermgrade'] = 'Midterm Grade Mode';
+ $string['finalgrade'] = 'Final Grade Mode';
+ $string['status'] = 'Import Status';
+ $string['activestatus'] = 'Import Status';
+ $string['title'] = 'Title';
+ $string['startime'] = 'Start Time';
+ $string['endtime'] = 'End Time';
+
+ 
+
+ 
+ 
